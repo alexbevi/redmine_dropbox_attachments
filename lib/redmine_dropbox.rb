781db5e785d7a3a617a2f2e150416d5e33d96aea
Alex Bevilacqua <alexbevi@gmail.com>
2013-04-01 14:49:48 -0400

    incorporate acts_as_attachable patch to avoid a weird filenotfound on heroku

--- lib/redmine_dropbox.rb
+++ lib/redmine_dropbox.rb
@@ -1,7 +1,23 @@
 require 'redmine_dropbox/attachment_patch'
 require 'redmine_dropbox/attachments_controller_patch'
-require 'redmine_dropbox/acts_as_attachable'
+
+module Redmine::Acts::Attachable
+  module InstanceMethods
+    alias_method :orig_save_attachments, :save_attachments
+    
+    def self.included(base)
+      base.extend(ClassMethods)
+    end
+
+    def save_attachments(attachments, author=User.current)
+      Attachment.set_context(self)
+      orig_save_attachments(attachments, author=User.current)
+    end
+    
+  end
+end
 
 ActiveRecord::Base.send(:include, Redmine::Acts::Attachable)
+
 AttachmentsController.send(:include, RedmineDropbox::AttachmentsControllerPatch)
 Attachment.send(:include, RedmineDropbox::AttachmentPatch)
