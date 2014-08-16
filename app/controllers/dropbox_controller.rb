797c8dc4ceda72351edaf6e73b67ea46a1bdb125
Alex Bevilacqua <alex@alexbevi.com>
2014-08-16 08:56:07 -0400

    fix RuntimeError (SSL Session wasn't initialized.)
    Fixes #21. Thanks a lot to @loganathan-rs for the patch

--- app/controllers/dropbox_controller.rb
+++ app/controllers/dropbox_controller.rb
@@ -3,8 +3,8 @@ class DropboxController < ApplicationController
 
   def authorize
     settings = Setting.find_by_name("plugin_#{PLUGIN_BASE}")
-    
-    if settings.nil?  
+
+    if settings.nil?
       flash[:warning] = l(:warning_settings_unsaved)
       redirect_to "/settings/plugin/#{PLUGIN_BASE}"
     elsif !params[:oauth_token]
@@ -16,11 +16,20 @@ class DropboxController < ApplicationController
       settings.value = tmp
       settings.save
 
+      # Store the token and secret in session to use in callback
+      session[:request_token] = request_token.token
+      session[:request_token_secret] = request_token.secret
+
       redirect_to request_token.authorize_url(:oauth_callback => url_for(:action => 'authorize'))
     else
+      # Create new request_token 
+      consumer = Dropbox::API::OAuth.consumer(:authorize)
+      request_token = OAuth::RequestToken.new(consumer, session[:request_token], session[:request_token_secret])
+
       tmp = settings.value
 
-      request_token = YAML::load(tmp["REQUEST_TOKEN"])
+      # comment below line to use new request_token
+      #request_token = YAML::load(tmp["REQUEST_TOKEN"])   
       access_token  = request_token.get_access_token(:oauth_verifier => params[:oauth_token])
 
       tmp["DROPBOX_TOKEN"]  = access_token.token
